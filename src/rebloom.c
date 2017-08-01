26cbefa75c9f09120196ef3a2d50e9b3a39ee68d
Mark Nunberg <mnunberg@haskalah.org>
2017-08-01 11:37:30 -0700

    Bump up rebloom version

--- src/rebloom.c
+++ src/rebloom.c
@@ -328,8 +328,10 @@ static int rsStrcasecmp(const RedisModuleString *rs1, const char *s2) {
         return REDISMODULE_ERR;                                                                    \
     } while (0);
 
+#define REBLOOM_VERSION 2
+
 int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-    if (RedisModule_Init(ctx, "bf", 1, REDISMODULE_APIVER_1) != REDISMODULE_OK) {
+    if (RedisModule_Init(ctx, "bf", REBLOOM_VERSION, REDISMODULE_APIVER_1) != REDISMODULE_OK) {
         return REDISMODULE_ERR;
     }
 
