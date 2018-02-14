548277c999ba3a8c1c4a92c961aa449013c37135
Mark Nunberg <mnunberg@haskalah.org>
2018-02-14 14:55:24 -0500

    Update murmurhash with 64 bit functions

--- contrib/MurmurHash2.c
+++ contrib/MurmurHash2.c
@@ -1,5 +1,6 @@
 //-----------------------------------------------------------------------------
-// MurmurHash2, by Austin Appleby
+// MurmurHash2 was written by Austin Appleby, and is placed in the public
+// domain. The author hereby disclaims copyright to this source code.
 
 // Note - This code makes a few assumptions about how your machine behaves -
 
@@ -12,53 +13,178 @@
 // 2. It will not produce the same results on little-endian and big-endian
 //    machines.
 
-unsigned int murmurhash2(const void * key, int len, const unsigned int seed)
-{
-	// 'm' and 'r' are mixing constants generated offline.
-	// They're not really 'magic', they just happen to work well.
+#include "murmurhash2.h"
+#define BIG_CONSTANT(x) (x##LLU)
 
-	const unsigned int m = 0x5bd1e995;
-	const int r = 24;
+//-----------------------------------------------------------------------------
 
-	// Initialize the hash to a 'random' value
+uint32_t MurmurHash2(const void *key, int len, uint32_t seed) {
+    // 'm' and 'r' are mixing constants generated offline.
+    // They're not really 'magic', they just happen to work well.
 
-	unsigned int h = seed ^ len;
+    const uint32_t m = 0x5bd1e995;
+    const int r = 24;
 
-	// Mix 4 bytes at a time into the hash
+    // Initialize the hash to a 'random' value
 
-	const unsigned char * data = (const unsigned char *)key;
+    uint32_t h = seed ^ len;
 
-	while(len >= 4)
-	{
-		unsigned int k = *(unsigned int *)data;
+    // Mix 4 bytes at a time into the hash
 
-		k *= m;
-		k ^= k >> r;
-		k *= m;
+    const unsigned char *data = (const unsigned char *)key;
 
-		h *= m;
-		h ^= k;
+    while (len >= 4) {
+        uint32_t k = *(uint32_t *)data;
 
-		data += 4;
-		len -= 4;
-	}
+        k *= m;
+        k ^= k >> r;
+        k *= m;
 
-	// Handle the last few bytes of the input array
+        h *= m;
+        h ^= k;
 
-	switch(len)
-	{
-	case 3: h ^= data[2] << 16;
-	case 2: h ^= data[1] << 8;
-	case 1: h ^= data[0];
-	        h *= m;
-	};
+        data += 4;
+        len -= 4;
+    }
 
-	// Do a few final mixes of the hash to ensure the last few
-	// bytes are well-incorporated.
+    // Handle the last few bytes of the input array
 
-	h ^= h >> 13;
-	h *= m;
-	h ^= h >> 15;
+    switch (len) {
+    case 3:
+        h ^= data[2] << 16;
+    case 2:
+        h ^= data[1] << 8;
+    case 1:
+        h ^= data[0];
+        h *= m;
+    };
 
-	return h;
+    // Do a few final mixes of the hash to ensure the last few
+    // bytes are well-incorporated.
+
+    h ^= h >> 13;
+    h *= m;
+    h ^= h >> 15;
+
+    return h;
+}
+
+//-----------------------------------------------------------------------------
+// MurmurHash2, 64-bit versions, by Austin Appleby
+
+// The same caveats as 32-bit MurmurHash2 apply here - beware of alignment
+// and endian-ness issues if used across multiple platforms.
+
+// 64-bit hash for 64-bit platforms
+
+uint64_t MurmurHash64A(const void *key, int len, uint64_t seed) {
+    const uint64_t m = BIG_CONSTANT(0xc6a4a7935bd1e995);
+    const int r = 47;
+
+    uint64_t h = seed ^ (len * m);
+
+    const uint64_t *data = (const uint64_t *)key;
+    const uint64_t *end = data + (len / 8);
+
+    while (data != end) {
+        uint64_t k = *data++;
+
+        k *= m;
+        k ^= k >> r;
+        k *= m;
+
+        h ^= k;
+        h *= m;
+    }
+
+    const unsigned char *data2 = (const unsigned char *)data;
+
+    switch (len & 7) {
+    case 7:
+        h ^= ((uint64_t)data2[6]) << 48;
+    case 6:
+        h ^= ((uint64_t)data2[5]) << 40;
+    case 5:
+        h ^= ((uint64_t)data2[4]) << 32;
+    case 4:
+        h ^= ((uint64_t)data2[3]) << 24;
+    case 3:
+        h ^= ((uint64_t)data2[2]) << 16;
+    case 2:
+        h ^= ((uint64_t)data2[1]) << 8;
+    case 1:
+        h ^= ((uint64_t)data2[0]);
+        h *= m;
+    };
+
+    h ^= h >> r;
+    h *= m;
+    h ^= h >> r;
+
+    return h;
+}
+
+// 64-bit hash for 32-bit platforms
+
+uint64_t MurmurHash64B(const void *key, int len, uint64_t seed) {
+    const uint32_t m = 0x5bd1e995;
+    const int r = 24;
+
+    uint32_t h1 = (uint32_t)(seed ^ len);
+    uint32_t h2 = (uint32_t)(seed >> 32);
+
+    const uint32_t *data = (const uint32_t *)key;
+
+    while (len >= 8) {
+        uint32_t k1 = *data++;
+        k1 *= m;
+        k1 ^= k1 >> r;
+        k1 *= m;
+        h1 *= m;
+        h1 ^= k1;
+        len -= 4;
+
+        uint32_t k2 = *data++;
+        k2 *= m;
+        k2 ^= k2 >> r;
+        k2 *= m;
+        h2 *= m;
+        h2 ^= k2;
+        len -= 4;
+    }
+
+    if (len >= 4) {
+        uint32_t k1 = *data++;
+        k1 *= m;
+        k1 ^= k1 >> r;
+        k1 *= m;
+        h1 *= m;
+        h1 ^= k1;
+        len -= 4;
+    }
+
+    switch (len) {
+    case 3:
+        h2 ^= ((unsigned char *)data)[2] << 16;
+    case 2:
+        h2 ^= ((unsigned char *)data)[1] << 8;
+    case 1:
+        h2 ^= ((unsigned char *)data)[0];
+        h2 *= m;
+    };
+
+    h1 ^= h2 >> 18;
+    h1 *= m;
+    h2 ^= h1 >> 22;
+    h2 *= m;
+    h1 ^= h2 >> 17;
+    h1 *= m;
+    h2 ^= h1 >> 19;
+    h2 *= m;
+
+    uint64_t h = h1;
+
+    h = (h << 32) | h2;
+
+    return h;
 }
