8949d26369914040aef5505f31fbbffa3a670e8e
ashtul <44112901+ashtul@users.noreply.github.com>
2019-05-14 15:40:45 +0300

    Rewritten Count-Min Sketch (#65)
    * add cms.c and countminsketch.h
    
    * init, increase and query funcs split
    
    * added to rebloom.c and updated Makefile
    
    * Fixed init by either dim or prob
    
    * Added CMS.MERGE
    
    * Copied CMS.DEBUG
    
    * replaced RMUtil_CreateFormattedString
    
    * Cleanup
    
    * Moved xxhash to conrtib
    
    * CountMinSketch_Commands.md started
    
    * Rewrite of Count-Min Sketch
    
    * removed unused files
    
    * Cleaning
    
    * Some python test for cms
    
    * Some Python tests and  fixes
    
    * python unittests
    
    * Additional testing
    
    * Define INNER_ERROR
    Removed RetainString
    
    * Additional changes after code review
    
    * test circleci
    
    * "redismodule.h" -> <redismodule.h>
    
    * 2nd
    
    * 3rd
    
    * Undoing change to config.yml
    
    * test yml
    
    * test
    
    * hardcoded config.yml
    
    * Major change - cmsCreate receives n, overEst & err
    
    * Update config.yml
    
    * Statistics study.c
    
    * Added cardinality
    
    * Automated study test
    
    * minor fix
    
    * fix for python test
    
    * Added cms.reserve key size Error prob err
    
    * Pull request comments fixes
    
    * RMUtil_ArgIndex instead of cmpCStrToRedisStr
    
    * bloom as cms
    
    * PR changes implemented
    
    * Eliminated errors
    
    * .
    
    * .

---
+++ tests/cms.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+from rmtest import ModuleTestCase
+from redis import ResponseError
+import sys
+from random import randint
+import math
+
+if sys.version >= '3':
+    xrange = range
+
+def TestSimple(self):
+    self.assertOk(self.cmd('cms.initbydim', 'cms1', '20', '5'))
+    self.assertOk(self.cmd('cms.incrby', 'cms1', 'a', '5'))
+    self.assertEqual([5L], self.cmd('cms.query', 'cms1', 'a'))
+#        self.assertEqual(['width', 20, 'depth', 5, 'count', 5], 
+#                         self.cmd('cms.info', 'cms1'))
+
+    self.assertOk(self.cmd('cms.initbyprob', 'cms2', '0.1', '0.1'))
+    self.assertOk(self.cmd('cms.incrby', 'cms2', 'a', '5'))
+    self.assertEqual([5L], self.cmd('cms.query', 'cms2', 'a'))
+#        self.assertEqual(['width', 20, 'depth', 4, 'count', 5], 
+#                         self.cmd('cms.info', 'cms2'))
+
+
+def TestMergeExt(self):
+    self.cmd('cms.initbydim', 'A', '2000', '10')
+    self.cmd('cms.initbydim', 'B', '2000', '10')
+    self.cmd('cms.initbydim', 'C', '2000', '10')
+    
+    itemsA = []
+    itemsB = []
+    for i in range(9000):
+        itemsA.append(randint(0, 100))
+        self.cmd('cms.incrby', 'A', i, itemsA[i])
+        itemsB.append(randint(0, 100))
+        self.cmd('cms.incrby', 'B', i, itemsB[i])    
+    self.assertOk(self.cmd('cms.merge', 'C', 2, 'A', 'B'))
+    for i in range(9000):
+        print(i, itemsA[i], self.cmd('cms.query', 'A', i), itemsB[i], self.cmd('cms.query', 'B', i), self.cmd('cms.query', 'C', i))
+#       print(itemsA[i], itemsB[i], self.cmd('cms.query', 'C', i))
+
+class CMSTest(ModuleTestCase('../rebloom.so')):
+    def test_simple(self):
+        self.assertOk(self.cmd('cms.initbydim', 'cms1', '20', '5'))
+        self.assertOk(self.cmd('cms.incrby', 'cms1', 'a', '5'))
+        self.assertEqual([5L], self.cmd('cms.query', 'cms1', 'a'))
+        self.assertEqual(['width', 20, 'depth', 5, 'count', 5], 
+                         self.cmd('cms.info', 'cms1'))
+
+        self.assertOk(self.cmd('cms.initbyprob', 'cms2', '1000', '0.001', '0.001'))
+        self.assertOk(self.cmd('cms.incrby', 'cms2', 'a', '5'))
+        self.assertEqual([5L], self.cmd('cms.query', 'cms2', 'a'))
+        self.assertEqual(['width', 2718, 'depth', 6, 'count', 5], 
+                         self.cmd('cms.info', 'cms2'))
+
+
+    def test_validation(self):
+        for args in (
+            (),
+            ('foo', ),
+            ('foo', '0.1'),
+            ('foo', '0.1', 'blah'),
+            ('foo', '10'),
+            ('foo', '10', 'blah'),
+            ('foo', 'blah', '10'),
+            ('foo', '0', '0'),
+            ('foo', '0', '100'),
+            ('foo', '100', '0'),
+        ):
+            self.assertRaises(ResponseError, self.cmd, 'cms.initbydim', *args)
+           
+        for args in (
+            (),
+            ('foo', ),
+            ('foo', '1000'),
+            ('foo', '0.1'),
+            ('foo', '1000', '0.1'),
+            ('foo', '0.1', 'blah'),
+            ('foo', '1000', '0.1', 'blah'),
+            ('foo', '10'),
+            ('foo', '1000', '10'),
+            ('foo', '10', 'blah'),
+            ('foo', '1000', '10', 'blah'),
+            ('foo', 'blah', '10'),
+            ('foo', '1000', 'blah', '10'),
+            ('foo', '0', '0'),
+            ('foo', '1000', '0', '0'),
+            ('foo', '0', '100'),
+            ('foo', '1000', '0', '100'),
+            ('foo', '100', '0'),
+            ('foo', '1000', '100', '0'),
+        ):         
+            self.assertRaises(ResponseError, self.cmd, 'cms.initbyprob', *args)
+
+        self.assertRaises(ResponseError, self.cmd, 'cms.initbydim', '0.1', '0.1')
+        self.assertRaises(ResponseError, self.cmd, 'cms.initbyprob', '10', '10')
+
+        self.assertOk(self.cmd('cms.initbydim', 'testDim', '100', '5'))
+        self.assertOk(self.cmd('cms.initbyprob', 'testProb', '1000', '0.1', '0.1'))
+       
+        for args in ((), ('test',)):
+            for cmd in ('cms.incrby', 'cms.query', 'cms.merge', 'cms.info'):
+                self.assertRaises(ResponseError, self.cmd, cmd, *args)
+    
+    def test_incrby_query(self):
+        self.cmd('cms.incrby', 'cms', 'bar', '5', 'baz', '42')
+        self.assertEqual([0], self.cmd('cms.query', 'cms', 'foo'))
+        self.assertEqual([0, 5, 42], self.cmd('cms.query',
+                                    'cms', 'foo', 'bar', 'baz'))
+        self.assertRaises(ResponseError, self.cmd, 'cms.incrby',
+                                    'cms', 'bar', '5', 'baz')
+        self.assertEqual([0, 5, 42], self.cmd('cms.query',
+                                    'cms', 'foo', 'bar', 'baz'))
+
+        c = self.client
+        self.assertOk(self.cmd('cms.incrby', 'test', 'foo', '1'))
+        self.assertEqual([1], self.cmd('cms.query', 'test', 'foo'))
+        self.assertEqual([0], self.cmd('cms.query', 'test', 'bar'))
+
+        self.assertOk(self.cmd('cms.incrby', 'test', 'foo', '1', 'bar', '1'))
+        for _ in c.retry_with_rdb_reload():
+            self.assertEqual([2], self.cmd('cms.query', 'test', 'foo'))
+            self.assertEqual([1], self.cmd('cms.query', 'test', 'bar'))
+            self.assertEqual([0], self.cmd('cms.query', 'test', 'nonexist'))
+    
+    def test_merge(self):
+        self.cmd('cms.initbydim', 'small_1', '20', '5')
+        self.cmd('cms.initbydim', 'small_2', '20', '5')
+        self.cmd('cms.initbydim', 'small_3', '20', '5')
+        self.cmd('cms.initbydim', 'large_4', '2000', '10')
+        self.cmd('cms.initbydim', 'large_5', '2000', '10')
+        self.cmd('cms.initbydim', 'large_6', '2000', '10')
+
+        # empty small batch
+        self.assertOk(self.cmd('cms.merge', 'small_3', 2, 'small_1', 'small_2'))
+        self.assertEqual(['width', 20, 'depth', 5, 'count', 0], 
+                         self.cmd('cms.info', 'small_3'))
+
+        # empty large batch
+        self.assertOk(self.cmd('cms.merge', 'large_6', 2, 'large_4', 'large_5'))
+        self.assertEqual(['width', 2000, 'depth', 10, 'count', 0], 
+                         self.cmd('cms.info', 'large_6'))
+
+        # non-empty small batch
+        self.cmd('cms.incrby', 'small_1', 'a', '21')
+        self.cmd('cms.incrby', 'small_2', 'a', '21')
+        self.assertOk(self.cmd('cms.merge', 'small_3', 2, 'small_1', 'small_2'))
+        self.assertEqual([42], self.cmd('cms.query', 'small_3', 'a'))
+                         
+        # non-empty small batch
+        self.cmd('cms.incrby', 'large_4', 'a', '21')
+        self.cmd('cms.incrby', 'large_5', 'a', '21')
+        self.assertOk(self.cmd('cms.merge', 'large_6', 2, 'large_4', 'large_5'))
+        self.assertEqual([42], self.cmd('cms.query', 'large_6', 'a'))
+  
+        # mixed batch
+        self.assertRaises(ResponseError, self.cmd, 'cms.merge', 'small_3', 2,
+                                    'small_2', 'large_5')
+    
+    def test_merge_extensive(self):
+        self.cmd('cms.initbydim', 'A', '2000', '10')
+        self.cmd('cms.initbydim', 'B', '2000', '10')
+        self.cmd('cms.initbydim', 'C', '2000', '10')
+        
+        itemsA = []
+        itemsB = []
+        for i in range(10000):
+            itemsA.append(randint(0, 100))
+            self.cmd('cms.incrby', 'A', str(i), itemsA[i])
+            itemsB.append(randint(0, 100))
+            self.cmd('cms.incrby', 'B', str(i), itemsB[i])    
+        self.assertOk(self.cmd('cms.merge', 'C', 2, 'A', 'B'))
+
+#        for i in range(10000):
+#            print(i, itemsA[i], self.cmd('cms.query', 'A', i), itemsB[i], self.cmd('cms.query', 'B', i), self.cmd('cms.query', 'C', i))
+#            print(itemsA[i], itemsB[i], self.cmd('cms.query', 'C', i))
+#        print(self.cmd('cms.info', 'A'))
+#        print(self.cmd('cms.info', 'B'))
+#        print(self.cmd('cms.info', 'C'))
+
+if __name__ == "__main__":
+    import unittest
+    unittest.main()
