9a3ddcf559324a3bcbdbba950d0217e5b7520e55
Ariel <ashtul@gmail.com>
2019-06-04 22:10:26 +0300

    tests

--- tests/topk.py
+++ tests/topk.py
@@ -92,11 +92,14 @@ class TopKTest(ModuleTestCase('../rebloom.so')):
         self.assertEqual('decay', info[6])
         self.assertAlmostEqual(0.9, float(info[7]))
 
-        self.client.retry_with_rdb_reload()
-
+        c = self.client
+        c.save()
+        c.dr.force_start()
+        c.dr.dump_and_reload()
         self.cmd('topk.reserve', 'test', '3', '50', '5', '0.9')
         self.cmd('topk.add', 'test', 'foo')
         self.assertEqual([None, 'foo', None], self.cmd('topk.list', 'test'))
+        self.assertEqual(2118, c.memory_usage('topk'))
 
     def test_time(self):
         self.cmd('topk.reserve', 'topk', '100', '1000', '5', '0.9')
