b0568a02e345d555bd19d8184e0e0c61b6aef728
Ariel <ashtul@gmail.com>
2019-07-10 17:52:53 +0300

    reduced use of pow()

--- src/topk.c
+++ src/topk.c
@@ -71,6 +71,11 @@ TopK *TopK_Create(uint32_t k, uint32_t width, uint32_t depth, double decay) {
     topk->data = TOPK_CALLOC(width * depth, sizeof(Bucket));
     topk->heap = TOPK_CALLOC(k, sizeof(HeapBucket));
 
+    topk->lookupTable = TOPK_CALLOC(TOPK_DECAY_LOOKUP_TABLE, sizeof(double));
+    for (int i = 0; i < TOPK_DECAY_LOOKUP_TABLE; ++i) {
+        topk->lookupTable[i] = pow(decay, i);
+    }
+
     return topk;
 }
 
@@ -137,7 +142,14 @@ char *TopK_Add(TopK *topk, const char *item, size_t itemlen, uint32_t increment)
         } else {
             uint32_t local_incr = increment;
             for(; local_incr > 0; --local_incr) {
-                double decay = pow(topk->decay, *countPtr);
+                double decay;
+                if (*countPtr < TOPK_DECAY_LOOKUP_TABLE) {
+                    decay = topk->lookupTable[*countPtr];
+                } else {
+                    //  using precalculate lookup table to save cpu
+                    decay = pow(topk->lookupTable[TOPK_DECAY_LOOKUP_TABLE - 1], (*countPtr / TOPK_DECAY_LOOKUP_TABLE) * 
+                            topk->lookupTable[*countPtr % TOPK_DECAY_LOOKUP_TABLE]);
+                }
                 double chance = rand() / (double)RAND_MAX;
                 if(chance < decay) {
                     --*countPtr;
