e8aecb7a11fade89309ad0ff3a66064d2b74f32d
Ariel <ashtul@gmail.com>
2019-09-08 08:08:40 +0300

    Init args tests

--- tests/pytests.py
+++ tests/pytests.py
@@ -5,21 +5,99 @@ import sys
 
 if sys.version >= '3':
     xrange = range
-
-class InitTestCase1(ModuleTestCase('../redisbloom.so', module_args=['INITIAL_SIZE', '400'])):
+class InitTestSize(ModuleTestCase('../redisbloom.so', module_args=['INITIAL_SIZE', '400'])):
     def test_fake(self):
         c, s = self.client, self.server
-        self.assertEqual(1, self.cmd('bf.add', 'test', 'foo'))
+        self.assertOk('OK', self.cmd('set', 'test', 'foo'))
 
-class InitTestCase2(ModuleTestCase('../redisbloom.so', module_args=['ERROR_RATE', '0.01'])):
+class InitTestError(ModuleTestCase('../redisbloom.so', module_args=['ERROR_RATE', '0.1'])):
     def test_fake(self):
         c, s = self.client, self.server
-        self.assertEqual(1, self.cmd('bf.add', 'test', 'foo'))
+        self.assertOk('OK', self.cmd('set', 'test', 'foo'))
 
-class InitTestCase3(ModuleTestCase('../redisbloom.so', module_args=['CF_MAX_EXPANSIONS', '512'])):
+class InitTestCFMaxExp(ModuleTestCase('../redisbloom.so', module_args=['CF_MAX_EXPANSIONS', '512'])):
     def test_fake(self):
         c, s = self.client, self.server
-        self.assertEqual(1, self.cmd('bf.add', 'test', 'foo'))
+        self.assertOk('OK', self.cmd('set', 'test', 'foo'))
+
+class InitTestCaseFailMissingArgs(ModuleTestCase('../redisbloom.so', module_args=['ONE_VAR'])):
+    def test_init_args(self):
+        try:
+            c, s = self.client, self.server
+        except Exception:
+            delattr(self, '_server')
+            self.assertOk('OK')
+        else:
+            self.assertOk('NotOK')
+
+class InitTestCaseFailWrongArgs(ModuleTestCase('../redisbloom.so', module_args=['MADEUP', 'VAR'])):
+    def test_init_args(self):
+        try:
+            c, s = self.client, self.server
+        except Exception:
+            delattr(self, '_server')
+            self.assertOk('OK')
+        else:
+            self.assertOk('NotOK')
+class InitTestCaseFailSize(ModuleTestCase('../redisbloom.so', module_args=['INITIAL_SIZE', '-1'])):
+    def test_init_args(self):
+        try:
+            c, s = self.client, self.server
+        except Exception:
+            delattr(self, '_server')
+            self.assertOk('OK')
+        else:
+            self.assertOk('NotOK')
+
+class InitTestCaseFailSizeStr(ModuleTestCase('../redisbloom.so', module_args=['INITIAL_SIZE', 'BF'])):
+    def test_init_args(self):
+        try:
+            c, s = self.client, self.server
+        except Exception:
+            delattr(self, '_server')
+            self.assertOk('OK')
+        else:
+            self.assertOk('NotOK')
+
+class InitTestCaseFailError(ModuleTestCase('../redisbloom.so', module_args=['ERROR_RATE', '-1'])):
+    def test_init_args(self):
+        try:
+            c, s = self.client, self.server
+        except Exception:
+            delattr(self, '_server')
+            self.assertOk('OK')
+        else:
+            self.assertOk('NotOK')
+
+class InitTestCaseFailErrorStr(ModuleTestCase('../redisbloom.so', module_args=['ERROR_RATE', 'BF'])):
+    def test_init_args(self):
+        try:
+            c, s = self.client, self.server
+        except Exception:
+            delattr(self, '_server')
+            self.assertOk('OK')
+        else:
+            self.assertOk('NotOK')
+
+class InitTestCaseFailCFMaxExp(ModuleTestCase('../redisbloom.so', module_args=['CF_MAX_EXPANSIONS', '-1'])):
+    def test_init_args(self):
+        try:
+            c, s = self.client, self.server
+        except Exception:
+            delattr(self, '_server')
+            self.assertOk('OK')
+        else:
+            self.assertOk('NotOK')
+
+class InitTestCaseFailCFMaxExpStr(ModuleTestCase('../redisbloom.so', module_args=['CF_MAX_EXPANSIONS', 'CF'])):
+    def test_init_args(self):
+        try:
+            c, s = self.client, self.server
+        except Exception:
+            delattr(self, '_server')
+            self.assertOk('OK')
+        else:
+            self.assertOk('NotOK')
 
 class RebloomTestCase(ModuleTestCase('../redisbloom.so')):
     def test_custom_filter(self):
