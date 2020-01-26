a56dc0e67cce4cc3c282c939b86416f4dea61cb5
Ariel <ashtul@gmail.com>
2020-01-26 13:46:19 +0200

    change murmur hash function to bloom function

--- contrib/hash.h
+++ contrib/hash.h
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include "murmurhash2.h"
 
 typedef struct {
   uint64_t a;
@@ -6,11 +7,9 @@ typedef struct {
 } TwoHash_t;
 
 
-uint64_t MurmurHash64A(const void *key, int len, unsigned int seed);
-
 inline void getTwoHash(TwoHash_t *hash, const void *key, int len) {
-  hash->a = MurmurHash64A(key, len, 0);
-  hash->b = MurmurHash64A(key, len, 1);
+  hash->a = MurmurHash64A_Bloom(key, len, 0);
+  hash->b = MurmurHash64A_Bloom(key, len, 1);
 }
 
 inline uint64_t getHash(TwoHash_t hash, int seed) {
