a06c2c0b0a9d64a0d4ab073653da7b7a0fe8e8ee
Ariel <ashtul@gmail.com>
2020-01-26 13:41:47 +0200

    cover rdb + info

--- tests/apbf.py
+++ tests/apbf.py
@@ -16,7 +16,7 @@ class APBFTest(ModuleTestCase('../redisbloom.so')):
         for i in range(len(items_list)):
             self.assertEqual([1L], self.cmd('apbf.exists apbf', items_list[i]))
         self.assertEqual([0L], self.cmd('apbf.exists apbf bloom'))
-
+    
     def test_1expiry(self):
         tests = 10000
         self.cmd('flushall')
@@ -34,6 +34,28 @@ class APBFTest(ModuleTestCase('../redisbloom.so')):
         for i in range(0, int(tests * 0.9)):
             positive += self.cmd('apbf.exists apbf', i)[0]
         self.assertLess(positive / (tests * 0.9), 0.015)
+    
+    def test_info(self):
+        self.cmd('flushall')
+        self.assertOk(self.cmd('apbf.reserve apbf 2 1000'))
+        results = ['Size', 4787L, 'Capacity', 1000L, 'Error rate', 2L, 
+                   'Inserts count', 0L, 'Hash functions count', 11L,
+                   'Periods count', 46L, 'Slices count', 57L]
+        self.assertEqual(results, self.cmd('apbf.info apbf'))
+
+    def test_rdb(self):
+        tests = 1000
+        self.cmd('flushall')
+        self.assertOk(self.cmd('apbf.reserve apbf 2', tests))
+        for i in range(tests * 3):
+            self.assertEqual('OK', self.cmd('apbf.insert apbf', i))
+        for i in range(int(tests * 2.1), tests * 3):
+            self.assertEqual(1, self.cmd('apbf.exists apbf', i)[0])
+
+#        self.client.retry_with_rdb_reload()
+        self.client.dr.dump_and_reload()
+        for i in range(int(tests * 2.1), tests * 3):
+            self.assertEqual(1, self.cmd('apbf.exists apbf', i)[0])
 
 if __name__ == "__main__":
     import unittest
