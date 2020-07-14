9b8e62de17100b497b56143cb8aa2f6fbe8d30c1
Guy Korland <gkorland@gmail.com>
2020-07-14 12:52:19 +0300

    Update gtm.js

--- docs/gtm.js
+++ docs/gtm.js
@@ -2,4 +2,4 @@
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N6FJFFK');
+})(window,document,'script','dataLayer','GTM-MDHLQC4');
