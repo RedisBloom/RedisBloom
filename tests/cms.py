cf0b2dd56f4dfada9ca15dd2f6be118c834bc803
Ariel Shtul <ashtul@gmail.com>
2021-05-09 09:39:51 +0300

    [BUG] Fix CMS.INCRBY negative or non-number (#303)
    * [BUG] Fix incrby negative or non-number
    
    * style
    
    (cherry picked from commit 1cd34ee846baeb9ec2106c85907a19b52e4724ce)

--- tests/cms.py
+++ tests/cms.py
@@ -137,6 +137,8 @@ class CMSTest(ModuleTestCase('../redisbloom.so')):
         self.assertOk(self.cmd('cms.initbydim', 'foo', '2000', '10'))
         self.assertOk(self.cmd('cms.initbydim', 'bar', '2000', '10'))
         self.assertOk(self.cmd('cms.initbydim', 'baz', '2000', '10'))
+        self.assertRaises(ResponseError, self.cmd, 'cms.incrby', 'foo', 'item', 'foo')
+        self.assertRaises(ResponseError, self.cmd, 'cms.incrby', 'foo', 'item', '-1')
         self.assertRaises(ResponseError, self.cmd, 'cms.merge', 'foo', 2, 'foo')
         self.assertRaises(ResponseError, self.cmd, 'cms.merge', 'foo', 'B', 3, 'foo')
         self.assertRaises(ResponseError, self.cmd, 'cms.merge', 'foo', 1, 'bar', 'weights', 'B')
