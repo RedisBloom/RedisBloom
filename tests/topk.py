816f9d73bbc44977371e46c9d6ad96c246698602
Ariel Shtul <ariel.shtul@redislabs.com>
2021-09-01 17:37:40 +0300

    add WITHCOUNT flag to TOPK.LIST  (#331)
    * add WITHCOUNT to TOPK.LIST
    
    * lint
    
    * memcpy instead of loop
    
    * Update src/rm_topk.c
    
    Co-authored-by: Omer Shadmi <76992134+oshadmi@users.noreply.github.com>
    
    * add benchmarks for cuckoo, cms, t-digest and topk
    
    * update benchmarks
    
    Co-authored-by: Omer Shadmi <76992134+oshadmi@users.noreply.github.com>
    (cherry picked from commit a435ae54ba315cf62116215126d5c0e985577fe9)

--- tests/topk.py
+++ tests/topk.py
@@ -100,7 +100,7 @@ class TopKTest(ModuleTestCase('../redisbloom.so')):
         self.assertEqual(['foo', 'baz'], self.cmd('topk.list', 'topk'))
         self.assertEqual([None, None, None, 'foo', None], 
                          self.cmd('topk.add', 'topk', 'bar', 'bar', 'bar', 'bar', 'bar'))
-        self.assertEqual(['baz', 'bar'], self.cmd('topk.list', 'topk'))
+        self.assertEqual(['bar', 'baz'], self.cmd('topk.list', 'topk'))
 
         self.assertRaises(ResponseError, self.cmd, 'topk.list', 'topk', '_topk_')
 
@@ -147,11 +147,20 @@ class TopKTest(ModuleTestCase('../redisbloom.so')):
         self.cmd('topk.add', 'topk', 'foo', 'baz', '42', 'foo', 'baz',)
         self.cmd('topk.add', 'topk', 'foo', 'bar', 'baz', 'foo', 'baz',)
         heapList = self.cmd('topk.list', 'topk')
-        self.assertEqual(['bar', 'foo', 'baz'], heapList)
+        self.assertEqual(['foo', 'baz', 'bar'], heapList)
 
         info = self.cmd('topk.info', 'topk')
         expected_info = ['k', 3L, 'width', 8L, 'depth', 7L, 'decay', '0.90000000000000002']
         self.assertEqual(expected_info, info)
+
+    def test_list_with_count(self):
+        self.cmd('FLUSHALL')
+        self.cmd('topk.reserve', 'topk', '3')
+        self.cmd('topk.add', 'topk', 'foo', 'bar', 'baz', '42', 'foo', 'bar', 'baz', )
+        self.cmd('topk.add', 'topk', 'foo', 'baz', '42', 'foo', )
+        self.cmd('topk.add', 'topk', 'foo', 'bar', 'baz', 'foo', )
+        heapList = self.cmd('topk.list', 'topk', 'WITHCOUNT')
+        self.assertEqual(['foo', 6, 'baz', 4, 'bar', 3], heapList)
 
 if __name__ == "__main__":
     import unittest
