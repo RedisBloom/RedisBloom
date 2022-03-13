e3ea530548a273ceea5a1ed8feab77d5ea48dccd
rafie <rafi@redislabs.com>
2022-03-13 17:29:38 +0200

    [2.2] Rocky Linux 8 and macOS M1

--- pack.sh
+++ pack.sh
@@ -74,6 +74,7 @@ export OSNICK=$($READIES/bin/platform --osnick)
 [[ $OSNICK == centos7 ]] && OSNICK=rhel7
 [[ $OSNICK == centos8 ]] && OSNICK=rhel8
 [[ $OSNICK == ol8 ]]     && OSNICK=rhel8
+[[ $OSNICK == rocky8 ]]  && OSNICK=rhel8
 
 export PRODUCT=redisbloom
 export PRODUCT_LIB=$PRODUCT.so
