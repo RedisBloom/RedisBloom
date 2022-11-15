bdc753e983aa1e23fea733d75b4e4e54848093c1
Ariel Shtul <ashtul@gmail.com>
2022-11-15 12:51:12 +0200

    [LICENSE] Change to RSALv2 and SSPLv1 (#585)
    * License
    
    * update headers and code with new license
    
    * deps/rmutil
    
    * fix lint
    
    * Update README and ramp.yml
    
    * Update LICENSE
    
    * rename LICENSE file
    
    Co-authored-by: Guy Korland <gkorland@gmail.com>

--- src/rm_tdigest.c
+++ src/rm_tdigest.c
@@ -1,3 +1,8 @@
+/*
+ * Copyright Redis Ltd. 2021 - present
+ * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
+ * the Server Side Public License v1 (SSPLv1).
+ */
 
 #include "rm_tdigest.h"
 #include "rmutil/util.h"
