f70b762866c4c0d2d1548d851fac354f7ea867b8
Rafi Einstein <rafi@redislabs.com>
2022-11-15 13:11:40 +0200

    [LICENSE] Change to RSALv2 and SSPLv1 (#585) (#587)
    * License
    
    * update headers and code with new license
    
    * deps/rmutil
    
    * fix lint
    
    * Update README and ramp.yml
    
    * Update LICENSE
    
    * rename LICENSE file
    
    Co-authored-by: Guy Korland <gkorland@gmail.com>
    (cherry picked from commit bdc753e983aa1e23fea733d75b4e4e54848093c1)
    
    Co-authored-by: Ariel Shtul <ashtul@gmail.com>

--- src/rm_topk.h
+++ src/rm_topk.h
@@ -1,3 +1,8 @@
+/*
+ * Copyright Redis Ltd. 2019 - present
+ * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
+ * the Server Side Public License v1 (SSPLv1).
+ */
 
 #pragma once
 
--- src/version.h
+++ src/version.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright Redis Ltd. 2017 - present
+ * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
+ * the Server Side Public License v1 (SSPLv1).
+ */
+
 #ifndef REBLOOM_VERSION_H_
 // This is where the modules build/version is declared.
 // If declared with -D in compile time, this file is ignored
