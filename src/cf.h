0087f479cc65f6308415f363302a66f871770108
Ariel Shtul <ashtul@gmail.com>
2022-11-15 11:19:11 +0200

    update headers and code with new license

--- src/cf.h
+++ src/cf.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright Redis Ltd. 2017 - present
+ * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
+ * the Server Side Public License v1 (SSPLv1).
+ */
+
 #ifndef CF_H
 #define CF_H
 #include "cuckoo.h"
