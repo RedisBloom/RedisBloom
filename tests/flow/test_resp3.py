97c415c9738f41f7f1ba46d818f1250e831c6d39
Ofir Moskovich <ofir.moskovich@redislabs.com>
2023-04-23 13:05:13 +0300

    test resp3

--- tests/flow/test_resp3.py
+++ tests/flow/test_resp3.py
@@ -8,5 +8,23 @@ class testResp3():
         env = self.env
         env.cmd('FLUSHALL')
         res = env.cmd('bf.add', 'test', 'item')
-        print(res)
+        assert res == True
 
+        res = env.cmd('bf.card', 'test')
+        assert res == 1
+
+        res = env.cmd('bf.EXISTS', 'test', 'item')
+        assert res == True
+
+        res = env.cmd('bf.info', 'test')
+        assert res == {b'Capacity': 100, b'Size': 240, b'Number of filters': 1,
+         b'Number of items inserted': 1, b'Expansion rate': 2}
+
+        res = env.cmd('bf.insert', 'test', 'ITEMS', 'item2', 'item3')
+        assert res == [True, True]
+
+        res = env.cmd('bf.madd', 'test', 'item4', 'item5')
+        assert res == [True, True]
+
+        res = env.cmd('bf.MEXISTS', 'test', 'item4', 'item5', 'item6')
+        assert res == [True, True, False]
