40ef409c9be7755d6507181528347379e12fb86f
Ofir Moskovich <ofir.moskovich@redislabs.com>
2023-04-27 15:16:11 +0300

    skip test for old redis

--- tests/flow/test_resp3.py
+++ tests/flow/test_resp3.py
@@ -1,11 +1,29 @@
 from common import *
 
+def is_redis_version_smaller_than(con, _version, is_cluster=False):
+    res = con.execute_command('INFO')
+    ver = ""
+    if is_cluster:
+        try:
+            ver = ((list(res.values()))[0])['redis_version']
+        except:
+            ver = res['redis_version']
+        #print(((list(res.values()))[0]))
+    else:
+        ver = res['redis_version']
+    return (version.parse(ver) < version.parse(_version))
+
+
 class testResp3():
     def __init__(self):
         self.env = Env(protocol=3)
 
     def test_bf_resp3(self):
         env = self.env
+        with env.getClusterConnectionIfNeeded() as r:
+            if(is_redis_version_smaller_than(r, "7.0.0")):
+                env.skip()
+
         env.cmd('FLUSHALL')
         res = env.cmd('bf.add', 'test', 'item')
         assert type(res) == bool
@@ -20,7 +38,7 @@ class testResp3():
 
         res = env.cmd('bf.info', 'test')
         assert res == {b'Capacity': 100, b'Size': 240, b'Number of filters': 1,
-         b'Number of items inserted': 1, b'Expansion rate': 2}
+            b'Number of items inserted': 1, b'Expansion rate': 2}
 
         res = env.cmd('bf.insert', 'test', 'ITEMS', 'item2', 'item3', 'item2')
         assert type(res[0]) == bool
@@ -71,15 +89,15 @@ class testResp3():
 
         res = env.cmd('cf.info a')
         assert res == {b'Size': 64, b'Number of buckets': 4,
-                       b'Number of filters': 1, b'Number of items inserted': 5,
-                       b'Number of items deleted': 1, b'Bucket size': 2,
-                       b'Expansion rate': 0, b'Max iterations': 20}
+                        b'Number of filters': 1, b'Number of items inserted': 5,
+                        b'Number of items deleted': 1, b'Bucket size': 2,
+                        b'Expansion rate': 0, b'Max iterations': 20}
 
         env.assertEqual(env.cmd('cms.initbyprob', 'cms2', '0.001', '0.01'), b'OK')
         env.assertEqual([5], env.cmd('cms.incrby', 'cms2', 'a', '5'))
         env.assertEqual([5], env.cmd('cms.query', 'cms2', 'a'))
         env.assertEqual({b'width': 2000, b'depth': 7, b'count': 5},
-                         env.cmd('cms.info', 'cms2'))
+                            env.cmd('cms.info', 'cms2'))
 
         env.assertEqual(env.cmd("tdigest.create", "tdigest"), b'OK')
         # independent of the datapoints this sketch has an invariant size after creation
@@ -87,10 +105,10 @@ class testResp3():
             env.assertEqual(env.cmd("tdigest.add", "tdigest", x), b'OK')
         td_info = env.cmd("tdigest.info", "tdigest")
         assert td_info == {b'Compression': 100, b'Capacity': 610,
-                           b'Merged nodes': 51, b'Unmerged nodes': 422,
-                           b'Merged weight': 9578, b'Unmerged weight': 422,
-                           b'Observations': 10000, b'Total compressions': 17,
-                           b'Memory usage': 9768}
+                            b'Merged nodes': 51, b'Unmerged nodes': 422,
+                            b'Merged weight': 9578, b'Unmerged weight': 422,
+                            b'Observations': 10000, b'Total compressions': 17,
+                            b'Memory usage': 9768}
 
         env.cmd('topk.reserve', 'topk', '2', '50', '5', '0.9')
         env.cmd('topk.add', 'topk', 'foo', 'bar', 'baz', '42', 'foo', 'bar', 'baz', )
