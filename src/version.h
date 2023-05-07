e1cbbefc1c5bd660268776aeadbe7db4df45e893
rafie <rafi@redislabs.com>
2023-05-07 08:53:51 +0300

    Version 2.6.0

--- src/version.h
+++ src/version.h
@@ -9,19 +9,17 @@
 // If declared with -D in compile time, this file is ignored
 
 #ifndef REBLOOM_VERSION_MAJOR
-#define REBLOOM_VERSION_MAJOR 99
+#define REBLOOM_VERSION_MAJOR 2
 #endif
 
 #ifndef REBLOOM_VERSION_MINOR
-#define REBLOOM_VERSION_MINOR 99
+#define REBLOOM_VERSION_MINOR 6
 #endif
 
 #ifndef REBLOOM_VERSION_PATCH
-#define REBLOOM_VERSION_PATCH 99
-
+#define REBLOOM_VERSION_PATCH 0
 #endif
 
 #define REBLOOM_MODULE_VERSION                                                                     \
     (REBLOOM_VERSION_MAJOR * 10000 + REBLOOM_VERSION_MINOR * 100 + REBLOOM_VERSION_PATCH)
-
 #endif
