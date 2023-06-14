829b83e44aa3c901b1e125d8b70b99a793d7739e
OfirMos <ofir.moskovich@redislabs.com>
2023-06-14 09:07:31 +0300

    Release 2 6 1 (#660)
    * Update Python tools (#657)
    
    (cherry picked from commit 6573d710728cbd2578e70157d75d67f8081c94fa)
    
    * add capability_name (#658)
    
    Co-authored-by: rafie <rafi@redislabs.com>
    (cherry picked from commit e5705eae9ca35d77bf8515072337746ed1d4bc49)
    
    * Update ramp.yml to Redis 7.2 (#659)
    
    (cherry picked from commit 1099b4bf70b5651cd1be500b4a9278debd4e6f8b)
    
    * bump version v2.6.1
    
    ---------
    
    Co-authored-by: Rafi Einstein <rafi@redislabs.com>
    Co-authored-by: Guy Korland <gkorland@gmail.com>

--- src/version.h
+++ src/version.h
@@ -17,7 +17,7 @@
 #endif
 
 #ifndef REBLOOM_VERSION_PATCH
-#define REBLOOM_VERSION_PATCH 0
+#define REBLOOM_VERSION_PATCH 1
 #endif
 
 #define REBLOOM_MODULE_VERSION                                                                     \
