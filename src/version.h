d774b567944adcfd713381311ec6b9ac259f6bee
YaacovHazan <yaacov.hazan@redis.com>
2024-01-10 22:25:55 +0200

    blabla

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 /*
- * Copyright Redis Ltd. 2017 - present
+ * asdasdCopyright Redis Ltd. 2017 - present
  * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
  * the Server Side Public License v1 (SSPLv1).
  */
