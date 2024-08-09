f0d80672c8b914d0e19dc2df14dc45d3863ab2ef
YaacovHazan <31382944+YaacovHazan@users.noreply.github.com>
2024-08-09 10:39:27 +0300

    see MOD-7532, bump version 2.6.14 (#19)
    (cherry picked from commit 44dce571d9377d93f4a5b7417c9c7db2dff2c90f)

--- src/version.h
+++ src/version.h
@@ -17,7 +17,7 @@
 #endif
 
 #ifndef REBLOOM_VERSION_PATCH
-#define REBLOOM_VERSION_PATCH 15
+#define REBLOOM_VERSION_PATCH 14
 #endif
 
 #define REBLOOM_MODULE_VERSION                                                                     \
