80e4407c4240dea46784430ca12aa83a9a5a3adc
YaacovHazan <31382944+YaacovHazan@users.noreply.github.com>
2024-08-09 10:39:12 +0300

    see MOD-7537, bump version 2.4.11 (#18)

--- src/version.h
+++ src/version.h
@@ -17,7 +17,7 @@
 #endif
 
 #ifndef REBLOOM_VERSION_PATCH
-#define REBLOOM_VERSION_PATCH 10
+#define REBLOOM_VERSION_PATCH 11
 #endif
 
 #define REBLOOM_MODULE_VERSION                                                                     \
