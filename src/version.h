d206104ff7ce0a3735800018c2bd653503517828
Eran Hadad <eranm22@gmail.com>
2024-09-03 13:52:14 +0000

    see MOD-7718, bump version 2.4.12

--- src/version.h
+++ src/version.h
@@ -17,7 +17,7 @@
 #endif
 
 #ifndef REBLOOM_VERSION_PATCH
-#define REBLOOM_VERSION_PATCH 11
+#define REBLOOM_VERSION_PATCH 12
 #endif
 
 #define REBLOOM_MODULE_VERSION                                                                     \
