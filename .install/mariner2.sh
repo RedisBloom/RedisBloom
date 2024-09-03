65442067ea24534f6cf91370efb15fb2d37f8d30
Eran Hadad <eranm22@gmail.com>
2024-09-03 08:26:58 +0000

    create build_package_req file and update ramp packer to 5.8

--- .install/mariner2.sh
+++ .install/mariner2.sh
@@ -6,7 +6,7 @@ git config --global --add safe.directory $PWD
 pip install --upgrade setuptools
 pip install -r tests/flow/requirements.txt
 
-pip install jinja2 ramp-packer  # required for packing the module (todo: move to pack.sh after refactor)
+pip install -r .install/build_package_requirments.txt  # required for packing the module (todo: move to pack.sh after refactor)
 
 # Install aws-cli for uploading artifacts to s3
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
