1beedc4a886f17ca575f433ac952a83c1639d03a
Meir Shpilraien (Spielrein) <meir@redis.com>
2024-09-10 15:53:00 +0300

    RED-134847 support active defrag on all probabilistic data types. (#25)
    The PR adds support for active defrag on all probabilistic data types. The implementation is strate forward, for each pointer in each datatype we call `RedisModule_DefragAlloc` to defrag the memory pointed by the poiter.
    
    Notice that as part of the changes, t-digest-c library was modified to use RedisModule Allocator for memory allocation. This is not the case before this PR and it was wrong, the memory allocation should be done via Redis allocator.
    
    Test was added to cover the new functionality.

--- src/common.h
+++ src/common.h
@@ -6,6 +6,12 @@
 
 #pragma once
 
+#include "redismodule.h"
 #if defined(DEBUG) || !defined(NDEBUG)
 #include "readies/cetara/diag/gdb.h"
 #endif
+
+static inline void *defragPtr(RedisModuleDefragCtx *ctx, void *ptr) {
+    void *tmp = RedisModule_DefragAlloc(ctx, ptr);
+    return tmp ? tmp : ptr;
+}
