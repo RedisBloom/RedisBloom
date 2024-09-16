167e74764f7806ad8dfe3837d6ef08e9703233d7
Meir Shpilraien (Spielrein) <meir@redis.com>
2024-09-16 14:12:48 +0300

    RED-134847 support active defrag on all probabilistic data types. (#25) (#28)
    The PR adds support for active defrag on all probabilistic data types. The implementation is strate forward, for each pointer in each datatype we call `RedisModule_DefragAlloc` to defrag the memory pointed by the poiter.
    
    Notice that as part of the changes, t-digest-c library was modified to use RedisModule Allocator for memory allocation. This is not the case before this PR and it was wrong, the memory allocation should be done via Redis allocator.
    
    Test was added to cover the new functionality.
    
    (cherry picked from commit 00cc08d2d3284f025bc18b650d41301619abb606)

--- src/rm_cms.c
+++ src/rm_cms.c
@@ -9,6 +9,7 @@
 
 #include "rmutil/util.h"
 #include "version.h"
+#include "common.h"
 
 #include <math.h>
 #include <stdlib.h>
@@ -303,6 +304,12 @@ void *CMSRdbLoad(RedisModuleIO *io, int encver) {
 
 void CMSFree(void *value) { CMS_Destroy(value); }
 
+static int CMSDefrag(RedisModuleDefragCtx *ctx, RedisModuleString *key, void **value) {
+    *value = defragPtr(ctx, *value);
+    CMSketch *cms = *value;
+    cms->array = defragPtr(ctx, cms->array);
+}
+
 size_t CMSMemUsage(const void *value) {
     CMSketch *cms = (CMSketch *)value;
     return sizeof(cms) + cms->width * cms->depth * sizeof(size_t);
@@ -315,7 +322,8 @@ int CMSModule_onLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
                                  .rdb_save = CMSRdbSave,
                                  .aof_rewrite = RMUtil_DefaultAofRewrite,
                                  .mem_usage = CMSMemUsage,
-                                 .free = CMSFree};
+                                 .free = CMSFree,
+                                 .defrag = CMSDefrag};
 
     CMSketchType = RedisModule_CreateDataType(ctx, "CMSk-TYPE", CMS_ENC_VER, &tm);
     if (CMSketchType == NULL)
