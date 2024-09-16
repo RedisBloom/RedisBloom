9c8a3b79918b7e1bb79d77f848d9e03237885c84
Victor Polevoy <fx@thefx.co>
2024-09-16 12:04:36 +0200

    Remove the commented code from the test_topk.py

--- tests/flow/test_topk.py
+++ tests/flow/test_topk.py
@@ -9,7 +9,6 @@ class testTopK():
         self.assertOk = self.env.assertTrue
         self.cmd = self.env.cmd
         self.assertEqual = self.env.assertEqual
-        self.assertContains = self.env.assertContains
         self.assertRaises = self.env.assertRaises
         self.assertTrue = self.env.assertTrue
         self.assertAlmostEqual = self.env.assertAlmostEqual
@@ -161,18 +160,12 @@ class testTopK():
     def test_no_init_params(self):
         self.cmd('FLUSHALL')
         self.cmd('topk.reserve', 'topk', '3')
-        # self.cmd('topk.add', 'topk', 'foo', 'bar', 'baz', '42', 'foo', 'bar', 'baz', )
-        # self.cmd('topk.add', 'topk', 'foo', 'baz', '42', 'foo', 'baz', )
-        # self.cmd('topk.add', 'topk', 'foo', 'bar', 'baz', 'foo', 'baz', )
         self.cmd('topk.add', 'topk', 'foo', 'bar', 'baz', '42', 'foo', 'bar', 'baz', 'foo', 'foo', 'foo', 'foo')
         self.cmd('topk.add', 'topk', 'foo', 'baz', '42', 'foo', 'baz', 'foo', 'foo', 'foo', 'foo', 'foo')
         self.cmd('topk.add', 'topk', 'foo', 'bar', 'baz', 'foo', 'baz', 'baz', 'baz', 'baz')
         heapList = self.cmd('topk.list', 'topk')
 
         self.assertEqual(['foo', 'baz', 'bar'], heapList)
-
-        # for item in ['foo', 'baz', 'bar']:
-        #     self.assertContains(item, heapList)
 
         info = self.cmd('topk.info', 'topk')
         expected_info = ['k', 3, 'width', 8, 'depth', 7, 'decay']
