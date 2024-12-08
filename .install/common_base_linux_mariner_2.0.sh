24855c692271a55fa5ad86d40a055bb84e763cfa
Eran Hadad <eranm22@gmail.com>
2024-12-08 19:14:53 +0000

    .

--- .install/common_base_linux_mariner_2.0.sh
+++ .install/common_base_linux_mariner_2.0.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-tdnf install -q -y build-essential git wget ca-certificates tar openssl-devel \
-    cmake python3 python3-pip rust clang which unzip
+tdnf install -y build-essential wget tar openssl-devel cmake python3 python3-pip which unzip
+git config --global --add safe.directory $PWD
 
-pip install -q --upgrade setuptools
-pip install -q --upgrade pip
-pip install -q -r tests/flow/requirements.txt
+pip install --upgrade setuptools
+pip install -r tests/flow/requirements.txt
 
-# These packages are needed to build the package
-pip install -q -r .install/build_package_requirements.txt
+pip install -r .install/build_package_requirements.txt  # required for packing the module (todo: move to pack.sh after refactor)
 
 # Install aws-cli for uploading artifacts to s3
-curdir="$PWD"
-cd /tmp/
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
-cd $curdir
