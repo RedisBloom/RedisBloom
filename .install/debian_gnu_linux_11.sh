f3304b045362513e6d18e02749547669989c5823
Eran Hadad <eranm22@gmail.com>
2024-12-08 19:01:37 +0000

    .

--- .install/debian_gnu_linux_11.sh
+++ .install/debian_gnu_linux_11.sh
@@ -5,4 +5,4 @@ MODE=$1 # whether to install using sudo or not
 
 $MODE apt update -qq
 $MODE apt upgrade -yqq
-$MODE apt-get install -y build-essential make cmake autoconf automake libtool lcov git wget zlib1g-dev lsb-release libssl-dev openssl ca-certificates python3 python3-pip curl unzip
+$MODE apt-get install -y build-essential make cmake autoconf automake libtool lcov git wget zlib1g-dev lsb-release libssl-dev openssl ca-certificates python3 python3-pip python3-venv curl unzip rsync libclang-dev clang
