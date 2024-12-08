367391558a15309d6a57172ac7334f09d7aa13ac
Eran Hadad <eranm22@gmail.com>
2024-12-08 19:49:42 +0000

    .

--- .install/ubuntu_18.04.sh
+++ .install/ubuntu_18.04.sh
@@ -6,7 +6,11 @@ MODE=$1 # whether to install using sudo or not
 $MODE apt update -qq
 $MODE apt upgrade -yqq
 
+add-apt-repository ppa:ubuntu-toolchain-r/test -y
+apt update
 apt-get install -y build-essential make autoconf automake libtool lcov git wget zlib1g-dev lsb-release libssl-dev openssl ca-certificates curl unzip gcc-10 g++-10 binfmt-support lsb-core awscli libclang-dev clang
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
+
 wget https://cmake.org/files/v3.28/cmake-3.28.0.tar.gz
 tar -xzvf cmake-3.28.0.tar.gz
 cd cmake-3.28.0
