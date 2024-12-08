ebab3589a890906dc7bab731d2d6713bad14bdc2
Eran Hadad <eranm22@gmail.com>
2024-12-08 19:52:53 +0000

    .

--- .install/amazon_linux_2.sh
+++ .install/amazon_linux_2.sh
@@ -3,13 +3,29 @@ MODE=$1 # whether to install using sudo or not
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
-yum -y install openssl-devel --allowerasing
-amazon-linux-extras install epel -y
-yum -y install epel-release yum-utils 
-yum-config-manager --add-repo http://vault.centos.org/centos/7/sclo/x86_64/rh/
-yum -y install gcc make cmake3 git python-pip openssl-devel bzip2-devel libffi-devel zlib-devel wget centos-release-scl scl-utils which tar unzip
-yum -y install devtoolset-11-gcc devtoolset-11-gcc-c++ devtoolset-11-make --nogpgcheck
-. scl_source enable devtoolset-11 || true
+$MODE yum update -y
+# Install the RPM package that provides the Software Collections (SCL) required for devtoolset-11
+$MODE yum install -y https://vault.centos.org/centos/7/extras/x86_64/Packages/centos-release-scl-rh-2-3.el7.centos.noarch.rpm
+
+# http://mirror.centos.org/centos/7/ is deprecated, so we changed the above link to `https://vault.centos.org`,
+# and we have to change the baseurl in the repo file to the working mirror (from mirror.centos.org to vault.centos.org)
+$MODE sed -i 's/mirrorlist=/#mirrorlist=/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo                        # Disable mirrorlist
+$MODE sed -i 's/#baseurl=http:\/\/mirror/baseurl=http:\/\/vault/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo # Enable a working baseurl
+
+$MODE yum install -y wget git which devtoolset-11-gcc devtoolset-11-gcc-c++ devtoolset-11-make \
+  rsync python3 unzip tar python3-devel python3-pip awscli
+
+source /opt/rh/devtoolset-11/enable
+
+cp /opt/rh/devtoolset-11/enable /etc/profile.d/scl-devtoolset-11.sh
+
+$MODE yum install -y curl
+$MODE yum install -y openssl11 openssl11-devel
+$MODE ln -s `which openssl11` /usr/bin/openssl
+
+# Install clang
+$MODE yum install -y clang
+
 make --version
 git --version
 wget https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz
@@ -25,9 +41,3 @@ python3 --version
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
-
-cp /opt/rh/devtoolset-11/enable /etc/profile.d/scl-devtoolset-11.sh
-
-yum install -y openssl11 openssl11-devel
-ln -s `which openssl11` /usr/bin/openssl
-yum install -y clang
