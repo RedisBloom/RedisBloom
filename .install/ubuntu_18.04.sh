0048ef1eb6f8675319ec0417e89c7e1929d44d07
Eran Hadad <eran.hadad@redis.com>
2024-12-24 19:45:45 +0200

    MOD-8197 Unified CI workflows (#826)
    - Updated and added new GitHub Actions workflows for build, test, and deployment
    - Introduced event-based workflows for specific triggers:
        - `event-ci.yml`: Runs CI checks on pull requests and pushes to specific branches
        - `event-nightly.yml`: Executes nightly builds and tests
        - `event-tag.yml`: Triggers workflows on new tags for release processes
    - Differentiated flow-based workflows for various environments:
        - `flow-alpine.yml`: Handles builds and tests for Alpine Linux
        - `flow-linux-x86.yml`: Manages workflows for x86 Linux distributions
        - `flow-macos.yml`: Manages workflows for macOS environments
        - `flow-ubuntu-arm.yml`: Handles builds and tests for Ubuntu on ARM architecture
    - Removed obsolete workflows
    - Added new installation scripts for various Linux distributions and macOS
    - Modified existing scripts and configurations for better compatibility and performance
    - Update GitHub Actions to use checkout@v4 and streamline Redis checkout process
    
    (cherry picked from commit 17c8fc3ba0b2609f3873439f0f7f48749a4dea92)

---
+++ .install/ubuntu_18.04.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+export DEBIAN_FRONTEND=noninteractive
+MODE=$1 # whether to install using sudo or not
+
+$MODE apt update -qq
+$MODE apt upgrade -yqq
+
+add-apt-repository ppa:ubuntu-toolchain-r/test -y
+apt update
+apt-get install -y build-essential make autoconf automake libtool lcov git wget zlib1g-dev lsb-release libssl-dev openssl ca-certificates curl unzip gcc-10 g++-10 binfmt-support lsb-core awscli libclang-dev clang libffi-dev
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
+
+wget https://cmake.org/files/v3.28/cmake-3.28.0.tar.gz
+tar -xzvf cmake-3.28.0.tar.gz
+cd cmake-3.28.0
+./configure
+make -j `nproc`
+make install
+cd ..
+ln -s /usr/local/bin/cmake /usr/bin/cmake
+wget https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz
+tar -xvf Python-3.9.6.tgz
+cd Python-3.9.6
+./configure
+make -j `nproc`
+make altinstall
+cd ..
+rm /usr/bin/python3 && ln -s `which python3.9` /usr/bin/python3
+rm /usr/bin/lsb_release
+python3 --version
+make --version
+cmake --version
+python3 -m pip install --upgrade pip
