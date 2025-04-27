bb39f35c5ca20f2c1228cdb00f2a6168a3276b01
Eran Hadad <eranm22@gmail.com>
2025-04-27 19:37:38 +0000

    add AGPLv3 license

--- src/topk.c
+++ src/topk.c
@@ -1,10 +1,15 @@
-/*
- * Copyright Redis Ltd. 2019 - present
- * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
- * the Server Side Public License v1 (SSPLv1).
+ /*
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
  *
+ * Licensed under your choice of (a) the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+ * 
  * Implementation by Ariel Shtul
+ * 
  */
+
 
 #include "topk.h"
 #include "murmur2/murmurhash2.h"
