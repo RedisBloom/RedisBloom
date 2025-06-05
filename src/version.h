c79dfcbcd14b5de907523da88c41fd3939bcc51e
Eran Hadad <eranm22@gmail.com>
2025-06-05 06:34:35 +0000

    initial commit 8.2

--- src/version.h
+++ src/version.h
@@ -12,15 +12,15 @@
 // If declared with -D in compile time, this file is ignored
 
 #ifndef REBLOOM_VERSION_MAJOR
-#define REBLOOM_VERSION_MAJOR 99
+#define REBLOOM_VERSION_MAJOR 8
 #endif
 
 #ifndef REBLOOM_VERSION_MINOR
-#define REBLOOM_VERSION_MINOR 99
+#define REBLOOM_VERSION_MINOR 2
 #endif
 
 #ifndef REBLOOM_VERSION_PATCH
-#define REBLOOM_VERSION_PATCH 99
+#define REBLOOM_VERSION_PATCH 0
 
 #endif
 
