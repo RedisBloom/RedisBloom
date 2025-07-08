5d50855aaa20af281bff141709752fc1c91f2c8a
efeldblum <109016390+ephraimfeldblum@users.noreply.github.com>
2025-07-08 15:11:05 +0300

    don't increment chain->nfilters until after successfully creating the filter (#49)
    (cherry picked from commit 1cd75aaff9c41f1c695130691da5ad5de97a55b8)

--- src/sb.c
+++ src/sb.c
@@ -34,11 +34,11 @@ static int SBChain_AddLink(SBChain *chain, uint64_t size, double error_rate) {
     *newlink = (SBLink){
         .size = 0,
     };
-    chain->nfilters++;
     int rc = bloom_init(&newlink->inner, size, error_rate, chain->options);
     if (rc != 0) {
         return rc == 1 ? SB_INVALID : SB_OOM;
     }
+    chain->nfilters++;
 
     return SB_SUCCESS;
 }
