4661795d58dcba2011aa4601914941641503d4ac
avivdavid23 <avivdavid2312@gmail.com>
2025-09-03 13:48:22 +0300

    MOD-10883 add topk.incrby info

--- tests/flow/test_docs_help.py
+++ tests/flow/test_docs_help.py
@@ -193,3 +193,17 @@ class testCommandDocsAndHelp():
             args=[('key', 'key'), ('item', 'string')],
             key_pos=1,
         )
+    
+    def test_command_docs_topk_incrby(self):
+        env = self.env
+        if server_version_less_than(env, '7.0.0'):
+            env.skip()
+        assert_docs(
+            env, 'topk.incrby',
+            summary='Increases the count of one or more items by increment',
+            complexity='O(n * k * incr) where n is the number of items, k is the depth and incr is the increment',
+            arity=-4,
+            since='2.0.0',
+            args=[('key', 'key'), ('item_increment', 'block')],
+            key_pos=1,
+        )
