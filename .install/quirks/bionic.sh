94b41506f06a16fd0947e9758716696d14375d57
TalBarYakar <talbaryakar@gmail.com>
2026-05-03 10:39:55 +0300

    replace gcc 10 with 8

--- .install/quirks/bionic.sh
+++ .install/quirks/bionic.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 #
-# Ubuntu 18.04 (bionic) extras: pulls a newer gcc-10 from the toolchain test
-# PPA (bionic only ships gcc-7) and builds cmake 3.28 from source because
-# bionic's apt cmake is too old for our CMakeLists. Mirrors the legacy
-# Dockerfile.bionic logic.
+# Ubuntu 18.04 (bionic) extras:
+#   - Enable Ubuntu universe and use gcc-8/g++-8 from official archives (no Launchpad PPA).
+#   - Build cmake 3.28 from source (bionic's apt cmake is too old).
 #
 # Sourced by install_script.sh; receives MODE as $1.
 
@@ -13,11 +12,12 @@ MODE="${1:-}"
 
 $MODE apt-get install -yqq --no-install-recommends \
     software-properties-common lsb-core binfmt-support cargo zlib1g-dev
-$MODE add-apt-repository ppa:ubuntu-toolchain-r/test -y
+
+$MODE add-apt-repository -y universe
 $MODE apt-get update -qq
-$MODE apt-get install -yqq --no-install-recommends gcc-10 g++-10
-$MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 \
-    --slave /usr/bin/g++ g++ /usr/bin/g++-10
+$MODE apt-get install -yqq --no-install-recommends gcc-8 g++-8
+$MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 80 \
+    --slave /usr/bin/g++ g++ /usr/bin/g++-8
 
 # bionic's cmake is too old; build the version pack.sh expects.
 cd /tmp
