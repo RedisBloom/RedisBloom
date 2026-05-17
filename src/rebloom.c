e578feba3d610e5f8283d87f4949e7f9b5e7414f
Tal Bar Yakar <62205470+TalBarYakar@users.noreply.github.com>
2026-05-17 11:16:16 +0300

    MOD-15418 - fix load rdb mem leak (#1007)
    * fix leak
    
    * use existing free funtion to free both

--- src/rebloom.c
+++ src/rebloom.c
@@ -1340,7 +1340,7 @@ static void *CFRdbLoad(RedisModuleIO *io, int encver) {
 
     bool err = false;
     CuckooFilter *cf = RedisModule_Calloc(1, sizeof(*cf));
-    errdefer(err, CuckooFilter_Free(cf));
+    errdefer(err, CFFree(cf));
     cf->numFilters = LoadUnsigned_IOError(io, err, NULL);
     cf->numBuckets = LoadUnsigned_IOError(io, err, NULL);
     if (cf->numFilters == 0 || cf->numBuckets == 0) {
