19d8bdd781bae92ef56138bfb27913275eb01c78
TalBarYakar <talbaryakar@gmail.com>
2026-06-25 16:08:28 +0300

    fix build for amazonlinux2

--- .install/os/amazonlinux2.sh
+++ .install/os/amazonlinux2.sh
@@ -24,6 +24,9 @@ $SUDO yum -y install --nogpgcheck --skip-broken \
 
 rhel_default_install
 $SUDO ln -sf "$(command -v cmake3)" /usr/bin/cmake
+$SUDO ln -sf /opt/rh/devtoolset-11/root/usr/bin/make /usr/local/bin/make
+$SUDO ln -sf /opt/rh/devtoolset-11/root/usr/bin/gcc /usr/local/bin/gcc
+$SUDO ln -sf /opt/rh/devtoolset-11/root/usr/bin/g++ /usr/local/bin/g++
 
 # Install aws-cli for uploading artifacts to s3
 install_aws_cli
